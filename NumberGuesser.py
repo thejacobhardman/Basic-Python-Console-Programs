acd26487a58971ec7a4e238cee2fc270b13c54a8
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2021-01-14 13:13:52 -0700

    create program files

---
+++ NumberGuesser.py
@@ -0,0 +1,24 @@
+################################################################################
+#                     Lab 1 – A Python Program                                 #
+#                                                                              #
+# PROGRAMMER:        [your name] [your email address]                          #
+# CLASS:             [CS160 or CS200]                                          #
+# TERM:              Spring 2021                                               #
+# INSTRUCTOR:        Dean Zeller                                               #
+# SUBMISSION DATE:   [date of submission]                                      #
+#                                                                              #
+# DESCRIPTION                                                                  #
+# [Include a brief but complete description of the problem the code is         #
+# solving.]                                                                    # 
+#                                                                              #
+# ORIGINAL SOURCE                                                              #
+# This code was originally written by:                                         #
+#    Author:   [writer of original program]                                    #
+#    Location: [indicate location, such as a website or textbook]              #
+#                                                                              #
+# COPYRIGHT:                                                                   #
+# This program is copyright (c)2021 [original-author-name], [your-name] and    #
+# Dean Zeller.                                                                 #
+#                                                                              #
+################################################################################
+
