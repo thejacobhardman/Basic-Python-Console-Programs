c99367e6bec43694d19fae26cbb396d5a7556313
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2021-01-15 10:12:56 -0700

    add new program file

---
+++ AvgWordLengthCalculator.py
@@ -0,0 +1,28 @@
+#################################################################################
+#                     Lab 1 – A Python Program                                  #
+#                                                                               #
+# PROGRAMMER:        Jacob Hardman hard7293@bears.unco.edu                      #
+# CLASS:             CS200                                                      #
+# TERM:              Spring 2021                                                #
+# INSTRUCTOR:        Dean Zeller                                                #
+# SUBMISSION DATE:   1/15/2021                                                  #
+#                                                                               #
+# DESCRIPTION                                                                   #
+# This program will calculate the average length of words based on user input.  #
+#                                                                               #
+# ORIGINAL SOURCE                                                               #
+# This code was originally written by:                                          #
+#    Author:   Jacob Hardman                                                    #
+#    Location: https://github.com/thejacobhardman/Basic-Python-Console-Programs #
+#                                                                               #
+# COPYRIGHT:                                                                    #
+# This program is copyright (c)2021 Jacob Hardman, and Dean Zeller              #
+#                                                                               #
+#################################################################################
+
+import os
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+
