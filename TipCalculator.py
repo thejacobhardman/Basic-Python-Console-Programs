38f9f7733f35e630e545aae90d3d6c9758a4ff8a
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2021-01-14 20:53:37 -0700

    finish tip calc

--- TipCalculator.py
+++ TipCalculator.py
@@ -4,7 +4,7 @@
 # PROGRAMMER:        [your name] [your email address]                          #
 # CLASS:             [CS160 or CS200]                                          #
 # TERM:              Spring 2021                                               #
-# INSTRUCTOR:        Dean Zeller                                               #
+# INSTRUCTOR:                                                     #
 # SUBMISSION DATE:   [date of submission]                                      #
 #                                                                              #
 # DESCRIPTION                                                                  #
@@ -18,7 +18,25 @@
 #                                                                              #
 # COPYRIGHT:                                                                   #
 # This program is copyright (c)2021 [original-author-name], [your-name] and    #
-# Dean Zeller.                                                                 #
+#                                                                  #
 #                                                                              #
 ################################################################################
 
+import os
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def Calculate_Tip(money_paid, tip_percent):
+    return money_paid * (tip_percent * 0.01)
+
+is_running = True
+while is_running:
+    cls()
+    print("Welcome To Tip Calculator!")
+
+    tip_amount = Calculate_Tip(money_paid=float(input("Please enter the amount of money that you paid as a decimal: ")), 
+                               tip_percent=float(input("Please enter the amount that you would like to tip as an integer. (10 = 10%): ")))
+
+    print("You've tipped $" + str(round(tip_amount, 2)) + ".")
+    input("Press any key to continue.")
